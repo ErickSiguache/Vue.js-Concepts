86eebb3ccae24faa90fbbe3d3addac281e66ab53
ErickSiguache <erickaguilars403@gmail.com>
2023-10-31 11:57:32 -0600

    feat: add the explain of the single file components and text interpolation files in the fundamentals folder

---
+++ Fundamentals/01-Single-File-Components.vue
@@ -0,0 +1,30 @@
+<!--
+    En Vue.js, los "single file components" (componentes de un solo archivo)
+    son una característica que permite definir componentes Vue en un solo
+    archivo. Tradicionalmente, las aplicaciones Vue.js se desarrollan
+    mediante componentes que se dividen en tres partes: la plantilla (HTML),
+    el script (JavaScript) y los estilos (CSS). Con los "single file
+    components", estas tres partes se combinan en un solo archivo con la
+    extensión .vue.
+
+    Ejemplo:
+-->
+
+<template>
+    <!-- La plantilla (HTML) del componente -->
+</template>
+
+<script>
+    // La lógica del componente (JavaScript)
+</script>
+
+<style>
+    /* Los estilos del componente (CSS) */
+</style>
+
+<!--
+    NOTA:
+
+    Cada uno de estos elementos es independiente, lo que significa que puedes
+    crear un archivo que contenga solo <script> o solo <template>.
+-->
---
+++ Fundamentals/02-Text-Interpolation.vue
@@ -0,0 +1,27 @@
+<!--
+    La interpolación de texto en Vue.js es una técnica que te permite mostrar
+    dinámicamente datos en el contenido HTML de tu aplicación. Se utiliza para
+    insertar valores de variables o propiedades de datos de Vue.js
+    directamente en el texto que se muestra en la interfaz de usuario.
+-->
+
+<script setup>
+    const name = "John Smith";
+</script>
+
+<template>
+    <!--
+        La interpolación de texto se realiza utilizando la doble llave ({{ }})
+        en las plantillas Vue.js. Por ejemplo, para mostrar la variable name
+        declarada anteriormente en la instancia Vue se hace de la siguiente
+        forma:
+    -->
+    <h1>{{ name }}</h1>
+</template>
+
+<!--
+    Nota: La interpolación de texto ({{ }}) en Vue.js funciona únicamente para
+    los valores internos de las etiquetas HTML. No se pueden utilizar
+    variables, por ejemplo, para asignar una clase. Para lograrlo, es necesario
+    utilizar la directiva v-bind.
+-->
