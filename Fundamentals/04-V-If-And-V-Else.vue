3e54982a7d0d51873573950efa315f85a80628c5
ErickSiguache <erickaguilars403@gmail.com>
2023-11-01 11:06:19 -0600

    feat: add the v-if, v-else if, v-else and v-show files in the fundamentals folder

---
+++ Fundamentals/04-V-If-And-V-Else.vue
@@ -0,0 +1,26 @@
+<!--
+    v-if y v-else son directivas condicionales en Vue.js que se utilizan para
+    mostrar o ocultar elementos en función de una expresión booleana. Estas
+    directivas son muy útiles para controlar la visibilidad de elementos en el
+    DOM basándose en el estado de los datos en tu instancia de Vue.
+-->
+
+<script setup>
+  const active = true;
+  const typePerson = "admin";
+</script>
+<template>
+    <!-- Uso del v-if -->
+    <p v-if="active"> I'm active </p>
+    <p v-if="!active"> I am not active </p>
+
+    <!--
+        v-else: La directiva v-else se utiliza junto con v-if y se coloca en
+        un elemento que sigue inmediatamente a un elemento con v-if. El
+        elemento con v-else se renderizará si la expresión en el v-if\
+        correspondiente es evaluada como falsa.
+    -->
+    <p v-if="typePerson === 'admin'"> I am an administrator </p>
+    <p v-else-if="typePerson === 'user'"> I am a user </p>
+    <p v-else> I am not an administrator</p>
+</template>
---
+++ Fundamentals/05-V-Show.vue
@@ -0,0 +1,37 @@
+<!--
+    v-show es otra directiva condicional en Vue.js, pero a diferencia de v-if,
+    v-show siempre renderea los elementos en el DOM. La diferencia clave
+    radica en la forma en que manejan la visibilidad.
+
+    v-show: La directiva v-show se utiliza para mostrar u ocultar un elemento
+    en el DOM basándose en una expresión booleana. Si la expresión es true, el
+    elemento se mostrará; si la expresión es false, el elemento se ocultará.
+    Sin embargo, el elemento siempre estará presente en el DOM, simplemente se
+    establecerá como display: none; para ocultarlo.
+-->
+
+<script setup>
+const name = "John Smith";
+const active = false;
+</script>
+
+<template>
+  <h1>Hello {{ name }}!</h1>
+  <h2 v-show="active">I'm in true</h2>
+</template>
+
+<!--
+    En resumen:
+
+    La diferencia clave entre v-if y v-show radica en cómo manejan la
+    manipulación del DOM. v-if elimina y recrea el elemento en el DOM según la
+    evaluación de la expresión, mientras que v-show simplemente manipula la
+    propiedad CSS display del elemento para mostrarlo u ocultarlo sin
+    eliminarlo del DOM.
+
+    v-if tiene un mayor costo de procesamiento al cambiar o alternar la
+    renderización inicial de un elemento en comparación con v-show. Por lo
+    tanto, es preferible utilizar v-show cuando necesite alternar algo con
+    mucha frecuencia. En contraste, es recomendable usar v-if si es poco
+    probable que la condición cambie durante el tiempo de ejecución.
+-->
