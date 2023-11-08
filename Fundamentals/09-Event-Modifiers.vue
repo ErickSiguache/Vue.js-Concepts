bfca07fe6836c8a32867f37029f2b7e44e4bc48e
ErickSiguache <erickaguilars403@gmail.com>
2023-11-08 11:46:11 -0600

    feat: add the event modifiers and click events files in the fundamentals folder

---
+++ Fundamentals/09-Event-Modifiers.vue
@@ -0,0 +1,43 @@
+<!--
+    Los modificadores de eventos en Vue.js son extensiones que puedes añadir a
+    las directivas v-on para alterar su comportamiento predeterminado. Los
+    modificadores se añaden como sufijos a las directivas v-on y se indican
+    con un punto (.) seguido del nombre del modificador. Aquí hay algunos
+    ejemplos de modificadores de eventos comunes: stop, capture, self, once,
+    passive y prevent.
+
+    En estos ejemplos de modificadores de eventos, se utilizará el modificador
+    "prevent" para ilustrar su funcionalidad:
+-->
+
+<script setup>
+    const greeting = (name) => {
+        console.log(`Hello ${name}`);
+    }
+</script>
+
+<template>
+    <!--
+        En el siguiente formulario, cada vez que se presiona el botón, el
+        formulario se recarga, lo que provoca la pérdida de información debido
+        a que se está renderizando nuevamente. Para evitar este comportamiento
+        y retener la información en el formulario, es necesario utilizar el
+        modificador "prevent".
+    -->
+    <form>
+      <button type="submit"> Click </button>
+    </form>
+    <!--
+        Prevent: Previene el comportamiento por defecto del evento. Equivale a
+        event.preventDefault() en JavaScript puro. Un ejemplo de uso es para
+        prevenir que una pagina se carge al momento de precionar un boton de un
+        formulario.
+    -->
+    <form v-on:submit.prevent="greeting('Hello')">
+        <button type="submit"> Click with prevent </button>
+    </form>
+    <!--
+        Nota: v-on:submit se utiliza de manera similar a click, pero
+        específicamente para botones del tipo "submit".
+    -->
+</template>
---
+++ Fundamentals/10-Click-Events.vue
@@ -0,0 +1,44 @@
+<!--
+    En Vue.js, es posible capturar eventos de clic de diferentes botones del
+    mouse, incluyendo el clic derecho, izquierdo y el botón central. Además,
+    se pueden manejar otros eventos como enter, tab, delete, esc, space, up,
+    down, left y right.
+
+    A continuación, se mostrarán ejemplos específicos para los eventos de
+    clic del mouse:
+-->
+
+<script setup>
+    const greeting = () => {
+        console.log("Hello World");
+    }
+</script>
+
+<template>
+    <!--
+        La directiva click Right en Vue.js se utiliza para manejar el evento
+        de clic derecho del mouse
+    -->
+    <button @click.right="greeting"> Click in Right Button </button>
+    <!--
+        NOTA: Como es bien sabido, el botón derecho del mouse generalmente se
+        utiliza para acceder a más opciones, lo que significa que cada vez que
+        se presiona este botón, se abre el menú contextual del navegador. Para
+        abordar este problema, podemos utilizar un modificador de eventos, en
+        este caso, "prevent", que evita que se realice el evento del clic
+        derecho.
+    -->
+    <button @click.right.prevent="greeting"> Right Click with prevent </button>
+    <!--
+        La directiva click Middle en Vue.js se utiliza para manejar el evento
+        de clic central del mouse
+    -->
+    <button @click.middle="greeting"> Middle Click </button>
+    <!--
+        La directiva click Left en Vue.js se utiliza para manejar el evento
+        de clic izquierdo del mouse, este evento es el evento por defecto
+        del v-on:click.
+    -->
+    <button @click.left="greeting"> Left Click </button>
+    <button @click="greeting"> Left Click by default </button>
+</template>
