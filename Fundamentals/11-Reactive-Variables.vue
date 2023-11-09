3cbe84f85d9c209cff6ed3b999f5045aebfe6c42
ErickSiguache <erickaguilars403@gmail.com>
2023-11-09 15:01:11 -0600

    feat: add the reactive variables and computed properties files in the basic folder

---
+++ Fundamentals/11-Reactive-Variables.vue
@@ -0,0 +1,66 @@
+<!--
+    La reactividad en el contexto de los frameworks de JavaScript, como Vue.js
+    y React, se refiere a la capacidad de los datos en una aplicación para
+    estar vinculados y actualizarse automáticamente cuando cambian.  Es una de
+    las características clave de estos frameworks y permite construir
+    interfaces de usuario dinámicas y altamente interactivas.
+
+    En el contexto de Vue.js y su sistema de reactividad, la reactividad
+    implica que cuando los datos cambian (por ejemplo, cuando una variable se
+    modifica), las partes relevantes de la interfaz de usuario que dependen
+    de esos datos también se actualizan automáticamente. Para crear variables
+    reactivas en Vue.js se hace uso de la función ref().
+
+    Ejemplo:
+
+    Las variables reactivas se llaman así porque son capaces de refrescar la
+    vista de los componente cuando éstas cambian. Para crear variables
+    reactivas en Vue.js podemos hacer uso de "ref". En Vue.js 3, ref() es una
+    nueva función proporcionada por el sistema de reactividad de Vue para
+    crear variables reactivas.
+-->
+
+<script setup>
+    // Importando Ref desde Vue.js para crear reactividad
+    import { ref } from "vue";
+
+    // Creando una variable reactiva
+    const message = ref("");
+    console.log(message);
+    /*
+        Output:
+            RefImpl
+                dep : Set(1) {ReactiveEffect}
+                __v_isRef : true
+                __v_isShallow : false
+                _rawValue : ""
+                _value : ""
+                value : (...)
+                [[Prototype]] : Object
+
+        Como se puede observar, ref() devuelve un objeto y entre las
+        propiedades que tiene, una de ellas es "value". Esta propiedad
+        contiene el valor de la variable, que en este caso se ha inicializado
+        como una cadena vacía (""). Por lo tanto, cuando deseamos cambiar el
+        valor de la variable, necesitamos hacer referencia a la propiedad
+        "value". Por ejemplo:
+    */
+
+    // Creando una función para cambiar el valor de forma reactiva.
+    const greeting = () => {
+        message.value = message.value === "" ? "Hello World" : "";
+    }
+</script>
+
+<template>
+    <!-- Usando la v-on:click para cambiar el valor de la variable -->
+    <button v-on:click="greeting"> Click Me </button>
+
+    <!--
+        NOTA: Cuando se renderiza el valor de una variable creada con ref(),
+        no es necesario utilizar "message.value" en el template. Vue.js
+        automáticamente reconoce que debe renderizar el valor asociado a la
+        propiedad "value".
+    -->
+    <h1>{{ message }}</h1>
+</template>
---
+++ Fundamentals/12-Computed-Properties.vue
@@ -0,0 +1,58 @@
+<!--
+    Las propiedades computadas en Vue.js son funciones que calculan y
+    devuelven un valor basado en las dependencias que tienen, como datos
+    reactivos o otras propiedades computadas.
+
+    Estas propiedades computadas son esenciales cuando se necesita realizar
+    operaciones basadas en datos reactivos y quieres mantener tu lógica de
+    la interfaz de usuario limpia y fácil de entender.
+
+    Ejemplo:
+-->
+
+<script setup>
+    // Importando computed desde Vue.js
+    import { ref, computed } from "vue";
+
+    // Creando la variable reactiva
+    const message = ref("");
+
+    // Creando una función computada para realizar cambios reactivos
+    const greeting = computed(() => {
+        message.value = "Hello World";
+    })
+</script>
+
+<template>
+    <!--
+        Llamando el evento v-on:click para ejecutar la función computada y
+        realizar cambios.
+    -->
+    <button v-on:click="greeting"> click </button>
+    <!-- Mostrando el mensaje -->
+    <h1>{{ msg }}</h1>
+</template>
+
+<!--
+    Como se puede observar, a simplevista no cambia nada con respecto a las
+    funciones comunes, sin embargo, esto no es asi; computed() almacena en
+    caché el resultado. Si "message" no cambia, "greeting" utilizará el
+    resultado anterior almacenado en caché, mejorando la eficiencia al evitar
+    recálculos innecesarios.
+
+    Cuando no se usa computed(), las función no tiene reactividad incorporada.
+    Si "message" cambia, "greeting" no se actualizará automáticamente. Se debe
+    llamar a esta función cada vez que quieras obtener el resultado actual, lo
+    que puede llevar a problemas de sincronización si no se gestiona
+    adecuadamente.
+
+    En resumen, la diferencia clave entre una propiedad calculada (computed())
+    y una propiedad definida como una "función común" radica en cómo se
+    gestionan y actualizan en función de sus dependencias reactivas. Las
+    propiedades calculadas se almacenan en caché, lo que significa que se
+    recalculan solo cuando alguna de sus dependencias cambia. Por lo tanto, es
+    altamente recomendable usar computed() cuando estás trabajando con
+    variables reactivas en Vue.js. Esto garantiza un rendimiento óptimo y una
+    gestión automática de las actualizaciones basadas en reactividad en la
+    aplicación.
+-->
